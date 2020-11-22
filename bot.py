13cecbb97aec90850960354bf05ff45937bf5748
Sabian Corrette <sabian1013@gmail.com>
2020-11-22 13:01:56 -0500

    Added sqlite3 and prefix changing

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 import os
 import logging
+import sqlite3
 
 from discord.ext import commands
 from dotenv import load_dotenv
+from contextlib import closing
+
+with closing(sqlite3.connect('guild_config.db')) as db:
+    with closing(db.cursor()) as c:
+        c.execute('''CREATE TABLE IF NOT EXISTS guild_settings
+                     (guild_id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+                      prefix TEXT)
+                  ''')
 
 def setup_logger(log_name, log_file, level=logging.WARNING):
     logger = logging.getLogger(log_name)
@@ -23,12 +32,52 @@ logger = setup_logger('discord', './logs/discord.log', logging.INFO)
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
+def set_prefix(bot, ctx):
+    prefix = '!'
 
-@bot.command(name='snom')
-async def snom(message):
+    with closing(sqlite3.connect('guild_config.db')) as db:
+        with closing(db.cursor()) as c:
+            c.execute('''SELECT prefix
+                         FROM guild_settings
+                         WHERE guild_id=?
+                      ''', (ctx.guild.id,))
+            try:
+                guild_prefix = c.fetchone()[0]
+            except:
+                guild_prefix = None
+    
+    return prefix if guild_prefix == None else guild_prefix
+
+bot = commands.Bot(command_prefix=set_prefix)
+
+@bot.command()
+async def prefix(ctx, prefix):
+    print("Attempting to change prefix for guild: ", ctx.guild.id, " to ", prefix)
+    with closing(sqlite3.connect('guild_config.db')) as db:
+        with closing(db.cursor()) as c:
+            c.execute('''SELECT *
+                         FROM guild_settings
+                         WHERE guild_id=?
+                      ''', (ctx.guild.id,))
+            if c.fetchone() == None:
+                print("Guild was not previously found in the database, adding both guild id and prefix")
+                c.execute('''INSERT INTO guild_settings
+                             VALUES (?, ?)
+                          ''', (ctx.guild.id, prefix,))
+                print("Successfully changed prefix to ", prefix, " for ", ctx.guild.id)
+            else:
+                print("Guild found previously in database, updating prefix")
+                c.execute('''INSERT INTO guild_settings
+                             VALUES (?)
+                             WHERE guild_id=?
+                          ''', (prefix, ctx.guild.id,))
+                print("Successfully changed prefix to ", prefix, " for ", ctx.guild.id)
+        db.commit()
+
+@bot.command()
+async def snom(ctx):
     msg = '<a:snomdance:779428824777228289>'
-    await message.channel.send(msg)
+    await ctx.channel.send(msg)
 
 @bot.event
 async def on_ready():
