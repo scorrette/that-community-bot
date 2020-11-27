0f5759cd88a93e740ee20b1ddc034bda8c5e289b
Sabian Corrette <sabian1013@gmail.com>
2020-11-27 18:09:06 -0500

    Added ping

--- cogs/info.py
+++ cogs/info.py
@@ -14,7 +14,12 @@ class Info(commands.Cog):
         minutes, seconds = divmod((uptime_now - self.uptime_start).total_seconds(), 60)
         hours, minutes = divmod(minutes, 60)
         days, hours = divmod(hours, 24)
-        await ctx.send(f'Up for {int(days)} days, {int(hours)} hours, {int(minutes)} minutes, and {seconds:.{2}f} seconds')
+        await ctx.send(f'Up for {int(days)} days, {int(hours)} hours, {int(minutes)} minutes, and {round(seconds, 2)} seconds')
+    
+    @commands.command(help="Latency check for the bot")
+    async def ping(self, ctx):
+        response_time = round(self.client.latency * 1000, 2)
+        await ctx.send(f'Pong! Response time {response_time} ms')
 
 def setup(client):
     client.add_cog(Info(client))
