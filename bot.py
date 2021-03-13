a1e05c7837ae53c7ce0b04519111461561250c5c
Jexkin <jexkin.cp@gmail.com>
2021-03-13 18:09:28 +0100

    moved everything into verify.py

--- bot.py
+++ bot.py
@@ -23,32 +23,6 @@ async def on_ready():
 #     if isinstance(error, commands.CommandNotFound):
 #         await ctx.channel.send(f'The command you entered does not exist. Use `{set_prefix(bot, ctx)[-1]}help` to see a list of commands')
 
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel('818537648515448872') #change logging channel ID
-    embed=discord.Embed(description=f"{member.mention} joined the server") #change helper role ID
-    await channel.send(embed=embed)
-
-@bot.event
-async def on_member_remove(member):
-    channel = bot.get_channel('818537648515448872') #change logging channel ID
-    embed=discord.Embed(description=f"{member.mention} left the server") #change helper role ID
-    await channel.send(embed=embed)
-
-@bot.event
-async def on_raw_reaction_add(reaction, member):
-    if reaction.emoji == 'thumbsup' and reaction.message.channel.id == 818537648515448872: #change channel ID 
-        member.add_role(818549791009144892, reason=None, atomic=True) #change role ID
-
-@bot.event
-async def on_member_update(before, after):
-    for role in after.roles:
-        if role.id == 818549791009144892: #change new-member role ID
-            channel = bot.get_channel('818537648515448872') #change verification channel ID
-            embed=discord.Embed(description=f"{after.mention}, welcome to Herphub! <@&479309565956063234>") #change helper role ID
-            await channel.send(embed=embed)
-    
-
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
--- cogs/verify.py
+++ cogs/verify.py
@@ -13,5 +13,30 @@ async def on_raw_reaction_add(self, payload):
     member = channel.guild.get_member(payload.user_id)
     emoji = payload.emoji
 
+@commands.Cog.listener()
+async def on_member_join(member):
+    channel = member.guild.get_channel('818537648515448872') #change logging channel ID
+    embed=discord.Embed(description=f"{member.mention} joined the server") #change helper role ID
+    await channel.send(embed=embed)
+
+@commands.Cog.listener()
+async def on_member_remove(member):
+    channel = member.guild.get_channel('818537648515448872') #change logging channel ID
+    embed=discord.Embed(description=f"{member.mention} left the server") #change helper role ID
+    await channel.send(embed=embed)
+
+@commands.Cog.listener()
+async def on_raw_reaction_add(reaction, member):
+    if reaction.emoji == 'thumbsup' and reaction.message.channel.id == 818537648515448872: #change to rules channel ID 
+        member.add_role(818549791009144892, reason=None, atomic=True) #change role ID
+
+@commands.Cog.listener()
+async def on_member_update(before, after):
+    for role in after.roles:
+        if role.id == 818549791009144892: #change new-member role ID
+            channel = member.guild.get_channel('818537648515448872') #change to rules channel ID
+            embed=discord.Embed(description=f"{after.mention}, welcome to Herphub! <@&479309565956063234>") #change helper role ID
+            await channel.send(embed=embed)
+
 def setup(bot):
     bot.add_cog(Verify(bot))
