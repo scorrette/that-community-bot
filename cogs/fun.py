df63c0514705cc46f61a0bc9a8de3fc3053e3e42
Sabian Corrette <sabian.corrette@gmail.com>
2021-03-18 20:39:34 -0400

    change 30s to 60s

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,7 +12,7 @@ class Fun(commands.Cog):
 
     @commands.command(help="Create a poll")
     async def poll(self, ctx):
-        embed = discord.Embed(title="Create a poll", description="The bot will proceed to ask you a series of questions. There is a 30 second timeout if the author does not respond.", color=0xba60f0) 
+        embed = discord.Embed(title="Create a poll", description="The bot will proceed to ask you a series of questions. There is a 60 second timeout if the author does not respond.", color=0xba60f0) 
         embed.add_field(name="Question 1", value="How long would you like the poll to be open?\nPlease use the following format: #d#h#m", inline=False)
         embed.add_field(name="Example", value="0d12h0m -> 12 hours\n2d12h0m -> 2 days and 12 hours\n0d0h30m -> 30 minutes", inline=False)
         await ctx.send(embed=embed)
@@ -21,10 +21,10 @@ class Fun(commands.Cog):
             return ctx.author == message.author
 
         try:
-            poll_time = await self.bot.wait_for('message', timeout=30, check=check)
+            poll_time = await self.bot.wait_for('message', timeout=60, check=check)
             while not re.match('\d{1,}d\d{1,}h\d{1,}m', poll_time.content):
                 await ctx.send("Incorrect time format. Please try again:")
-                poll_time = await self.bot.wait_for('message', timeout=30, check=check)
+                poll_time = await self.bot.wait_for('message', timeout=60, check=check)
             
             time_vals = re.split('[dhm]+', poll_time.content)
             end_time_delta = timedelta(days=int(time_vals[0]), hours=int(time_vals[1]), minutes=int(time_vals[2]))
