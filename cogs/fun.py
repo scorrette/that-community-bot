45a16544a98bc6f48e6261484280bf634b6ef611
Sabian Corrette <sabian.corrette@gmail.com>
2021-03-18 20:36:34 -0400

    get poll time

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,9 @@
 import discord
 import asyncio
+import re
 
+from pytz import timezone
+from datetime import datetime, timedelta
 from discord.ext import commands
 
 class Fun(commands.Cog):
@@ -9,11 +12,23 @@ class Fun(commands.Cog):
 
     @commands.command(help="Create a poll")
     async def poll(self, ctx):
-        await ctx.send('Enter how long the poll should be open (h for hours, m for minutes, s for seconds. Ex: 1h30m, 10m, etc.):')
+        embed = discord.Embed(title="Create a poll", description="The bot will proceed to ask you a series of questions. There is a 30 second timeout if the author does not respond.", color=0xba60f0) 
+        embed.add_field(name="Question 1", value="How long would you like the poll to be open?\nPlease use the following format: #d#h#m", inline=False)
+        embed.add_field(name="Example", value="0d12h0m -> 12 hours\n2d12h0m -> 2 days and 12 hours\n0d0h30m -> 30 minutes", inline=False)
+        await ctx.send(embed=embed)
+
+        def check(message):
+            return ctx.author == message.author
 
         try:
-            poll_time = await self.bot.wait_for('message', timeout=30)
-            await ctx.send(poll_time)
+            poll_time = await self.bot.wait_for('message', timeout=30, check=check)
+            while not re.match('\d{1,}d\d{1,}h\d{1,}m', poll_time.content):
+                await ctx.send("Incorrect time format. Please try again:")
+                poll_time = await self.bot.wait_for('message', timeout=30, check=check)
+            
+            time_vals = re.split('[dhm]+', poll_time.content)
+            end_time_delta = timedelta(days=int(time_vals[0]), hours=int(time_vals[1]), minutes=int(time_vals[2]))
+            await ctx.send(datetime.today() + end_time_delta)
         except asyncio.TimeoutError:
             await ctx.send("You took too long, cancelling.")
 
