e197d7fc146eb2c3cd0abd5704375533e15a8822
Sabian Corrette <sabian.corrette@gmail.com>
2021-03-19 22:23:37 -0400

    Add option portion of poll

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,8 +12,10 @@ class Fun(commands.Cog):
 
     @commands.command(help="Create a poll")
     async def poll(self, ctx):
+        emojis = [':one:', ':two:', ':three:', ':four:', ':five:', ':six:', ':seven:', ':eight:', ':nine:']
+
         embed = discord.Embed(title="Create a poll", description="The bot will proceed to ask you a series of questions. There is a 60 second timeout if the author does not respond.", color=0xba60f0) 
-        embed.add_field(name="Question 1", value="How long would you like the poll to be open?\nPlease use the following format: #d#h#m", inline=False)
+        embed.add_field(name="Question", value="How long would you like the poll to be open?\nPlease use the following format: #d#h#m", inline=False)
         embed.add_field(name="Example", value="0d12h0m -> 12 hours\n2d12h0m -> 2 days and 12 hours\n0d0h30m -> 30 minutes", inline=False)
         await ctx.send(embed=embed)
 
@@ -28,7 +30,37 @@ class Fun(commands.Cog):
             
             time_vals = re.split('[dhm]+', poll_time.content)
             end_time_delta = timedelta(days=int(time_vals[0]), hours=int(time_vals[1]), minutes=int(time_vals[2]))
-            await ctx.send(datetime.today() + end_time_delta)
+
+            embed = discord.Embed(title=None, description="Enter between 2 to 9 options, one line at a time.\nType **done** to complete your choices.", color=0xba60f0)
+            embed.add_field(name="Example", value="pie\ncake\nice cream\nfruit\n**done**", inline=False)
+            await ctx.send(embed=embed)
+
+            option = await self.bot.wait_for('message', timeout=60, check=check)
+            options = [option.content]
+            while len(options) < 9:
+                if options[-1].lower() == 'done':
+                    options.pop()
+                    if len(options) < 2:
+                        await ctx.send('Not enough options, quitting prematurely...')
+                        return
+                    else:
+                        break
+                else:
+                    option = await self.bot.wait_for('message', timeout=60, check=check)
+                    options.append(option.content)
+
+            embed = discord.Embed(title=None, description=None, color=0xba60f0)
+
+            body = ''
+            for i in range(len(options)):
+                body += f'{emojis[i]} {options[i]}'
+                if not i == len(options) - 1:
+                    body += '\n'
+
+            embed.add_field(name="Options", value=body, inline=False)
+            await ctx.send(embed=embed)
+
+            end_time = datetime.today() + end_time_delta
         except asyncio.TimeoutError:
             await ctx.send("You took too long, cancelling.")
 
