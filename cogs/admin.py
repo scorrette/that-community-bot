d16443f0b6f8562982bc39bcbfcba5e8e2f3494a
Sabian Corrette <sabian.corrette@gmail.com>
2021-03-28 18:39:40 -0400

    start beginning of prefix command

--- cogs/admin.py
+++ cogs/admin.py
@@ -6,5 +6,18 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(help="Modify server prefixes")
+    async def prefix(self, ctx, option, prefix = None):
+        if option.lower() == 'add':
+            if prefix == None:
+                await ctx.send('This command expects a prefix to be passed to it.')
+        elif option.lower() == 'list':
+            print('bad')
+        elif option.lower() == 'remove':
+            if prefix == None:
+                await ctx.send('This command expects a prefix to be passed to it.')
+        else:
+            print('bad')
+
 def setup(bot):
     bot.add_cog(Admin(bot))
