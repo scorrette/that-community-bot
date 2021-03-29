304c3842040f4fdb13dd1051531a828571f75973
Sabian Corrette <sabian.corrette@gmail.com>
2021-03-28 20:06:35 -0400

    add prefix list

--- cogs/admin.py
+++ cogs/admin.py
@@ -14,6 +14,22 @@ class Admin(commands.Cog):
 
         await ctx.send(f'`{prefix}` has been added to the prefix list.')
 
+    async def list_prefixes(self, ctx):
+        desc = "Below is a list of prefixes set for your server:\n"
+        embed = discord.Embed(title="Prefixes", description=desc, color=0xba60f0)
+
+        async with self.bot.pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(f'SELECT `prefix` FROM `prefixes` WHERE `guild_id`={ctx.guild.id}')
+                prefixes = await cur.fetchall()
+
+                for i in range(len(prefixes)):
+                    desc += prefixes[i]
+                    if not i == len(prefixes) - 1:
+                        desc += '\n'
+
+        await ctx.send(embed=embed)
+
     async def remove_prefix(self, ctx, prefix):
         async with self.bot.pool.acquire() as conn:
             async with conn.cursor() as cur:
@@ -29,13 +45,13 @@ class Admin(commands.Cog):
                 await ctx.send('This command expects a prefix to be passed to it.')
             else: await add_prefix(self, ctx, prefix)
         elif option.lower() == 'list':
-            return
+            await list_prefixes(self, ctx)
         elif option.lower() == 'remove':
             if prefix == None:
                 await ctx.send('This command expects a prefix to be passed to it.')
             else: await remove_prefix(self, ctx, prefix)
         else:
-            print('bad')
+            await ctx.send('You must provide either add, list, or remove as an option.')
 
 def setup(bot):
     bot.add_cog(Admin(bot))
